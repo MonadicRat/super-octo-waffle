a18058e3b4e989d121b5243d72815701eb864e46
MonadicRat <65781921+MonadicRat@users.noreply.github.com>
2022-01-05 11:47:11 +0300

    Update GSEA.R

--- GSEA.R
+++ GSEA.R
@@ -3,13 +3,19 @@ library(dplyr)
 library(fgsea)
 
 setwd("/media/yanus/3ce7ab71-4bf7-4250-894c-508fb7a28109/Meth/")
-genes <- read.csv("/media/yanus/3ce7ab71-4bf7-4250-894c-508fb7a28109/Meth/genes_and_sites.csv",
+genes <- read.csv("genes_and_sites.csv",
                   header = T)[2:8]
+
+print("[GSEA] Gene set loaded")
 
 genes <- genes %>% 
   dplyr::filter(P < 0.05 & !is.na(Gene)) %>%
   group_by(Gene) %>% 
   summarise(Avg_Rho = mean(Rho))
+
+print("[GSEA] Genes filtered and summarised")
+
+write.csv(genes, "genes.rhos.filtered.csv", quote = F)
 
 GOBP <- dplyr::filter(
   msigdbr(species = "Homo sapiens", category = "C5", subcategory = "GO:BP"), 
@@ -23,9 +29,21 @@ GOCC <- dplyr::filter(
   msigdbr(species = "Homo sapiens", category = "C5", subcategory = "GO:CC"), 
   gene_symbol %in% genes$Gene)
 
-ranks <- list(genes$Gene, genes$Avg_Rho)
+print("[GSEA] Databases loaded")
 
-categories_list <- list()
-for (i in 1:length(categories)) {
-  categories_list[[i]] <- categories[[i]] %>% as.list()
-}
+ranks <- genes$Avg_Rho %>% as.numeric()
+names(ranks) <- genes$Gene
+
+grouped_categories <- categories %>% group_by(gs_exact_source)
+pathways <- group_map(grouped_categories, ~ .x[[1]])
+names(pathways) <- group_data(grouped_categories)[[1]]
+
+print("[GSEA] Preparations done. Starting GSEA")
+
+gsea <- fgsea(pathways, ranks)
+gsea$leadingEdge <- vapply(gsea$leadingEdge, paste, collapse = ";", character(1L))
+
+print("[GSEA] GSEA done")
+
+write.csv(gsea, "gsea_go.csv", quote = F)
+print("[GSEA] File written. Done")
