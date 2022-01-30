aa8bf5d5ab6acd91b61db216e3d3df47494df539
MonadicRat <65781921+MonadicRat@users.noreply.github.com>
2022-01-30 11:03:46 +0300

    Update CalculatePearsons1.1.0.R

--- CalculatePearsons1.1.0.R
+++ CalculatePearsons1.1.0.R
@@ -1,25 +1,29 @@
-library(ChAMP)
+#library(ChAMP)
 library(magrittr)
-library(stringr)
+#library(stringr)
 library(dplyr)
+library(bigmemory)
 
 setwd("/media/yanus/3ce7ab71-4bf7-4250-894c-508fb7a28109/Meth/")
-dimensions <- c(100, 100)
-betas <- read.table("GSE40279_average_beta_GSM989827-GSM989990.sample.txt", header = T, sep = "\t", row.names = 1)
+options(bigmemory.allow.dimnames = T)
+dimensions <- c(473034, 656)
+betas <- read.big.matrix("GSE40279_average_beta_GSM989827-GSM989990.sample.txt", 
+                         sep = "\t", header = T, type = "float", 
+                         ignore.row.names = F, has.row.names = T)
 print("[CalculatePearsons] Beta values loaded")
 
-myNorm <- champ.norm(beta = betas, method = "PBC")
+#myNorm <- champ.norm(beta = betas, method = "PBC")
 print("[CalculatePearsons] Normalization done")
-age <- read.table("age.sample.txt", sep = "\t", header = F)
+age <- read.table("age.txt", sep = "\t", header = F)
 
-coeffs <- matrix(nrow = dimensions[1], ncol = 2)
-dimnames(coeffs) <- list(rownames(myNorm), c("Rho", "p"))
+coeffs <- big.matrix(nrow = dimensions[1], ncol = 2)
+dimnames(coeffs) <- list(rownames(fuck), c("Rho", "p"))
 for (r in 1:dimensions[1]){
-  coeff <- cor.test(as.vector(age$V2), as.vector(as.numeric(myNorm[r,])))
+  coeff <- cor.test(as.vector(age$V2), as.vector(as.numeric(fuck[r,])))
   coeffs[r,1] <- coeff$estimate
   coeffs[r,2] <- coeff$p.value
 } 
 print("[CalculatePearsons] Coefficients calculated")
 
-write.table(coeffs, "coeffs.sample.txt", quote = F, sep = "\t")
+write.big.matrix(coeffs, "coeffs.txt", sep = "\t", col.names = T, row.names = T)
 print("[CalculatePearsons] File written. Done")
