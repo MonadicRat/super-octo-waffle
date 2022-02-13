def9afdc2fb1ad961adc805e74a9f6a7cde32da0
MonadicRat <65781921+MonadicRat@users.noreply.github.com>
2022-02-13 09:18:43 +0300

    Update GSEA_pathways.R

--- GSEA_pathways.R
+++ GSEA_pathways.R
@@ -3,8 +3,15 @@ library(dplyr)
 library(fgsea)
 
 setwd("/media/yanus/3ce7ab71-4bf7-4250-894c-508fb7a28109/Meth/")
-genes <- read.csv("genes.rhos.filtered.csv")[2:3]
-print("[GSEA KEGG] Gene set loaded")
+genes <- read.csv("genes_and_sites.noreps.csv",
+                  header = T)[3:9]
+
+print("[GSEA] Gene set loaded")
+
+genes <- genes %>% 
+  dplyr::filter(P < 10^-10 & !is.na(Gene)) %>%
+  group_by(Gene) %>% 
+  summarise(Avg_Rho = mean(Rho))
 
 KEGG <- dplyr::filter(
   msigdbr(species = "Homo sapiens", category = "C2", subcategory = "CP:KEGG"), 
@@ -15,7 +22,7 @@ ranks <- genes$Avg_Rho %>% as.numeric()
 names(ranks) <- genes$Gene
 
 grouped_categories <- KEGG %>% group_by(gs_exact_source)
-pathways <- group_map(grouped_categories, ~ .x[[1]])
+pathways <- group_map(grouped_categories, ~ .x[[4]])
 names(pathways) <- group_data(grouped_categories)[[1]]
 print("[GSEA KEGG] Preparations done. Starting GSEA")
 
@@ -23,5 +30,8 @@ gsea <- fgsea(pathways, ranks)
 gsea$leadingEdge <- vapply(gsea$leadingEdge, paste, collapse = ";", character(1L))
 print("[GSEA KEGG] GSEA done")
 
-write.csv(gsea, "gsea_go.csv", quote = F)
+gsea_pval <- gsea %>% dplyr::filter(padj < 0.05)
+
+write.csv(gsea, "gsea_kegg.csv", quote = F)
+write.csv(gsea_pval, "gsea_kegg.pval.csv", quote = F)
 print("[GSEA KEGG] File written. Done")
