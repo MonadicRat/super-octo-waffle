0ce9340819c0d172b09f9c17402a7bc294f717c0
MonadicRat <65781921+MonadicRat@users.noreply.github.com>
2022-02-13 09:18:13 +0300

    Update GSEA.R

--- GSEA.R
+++ GSEA.R
@@ -1,15 +1,16 @@
 library(msigdbr)
 library(dplyr)
 library(fgsea)
+library(magrittr)
 
 setwd("/media/yanus/3ce7ab71-4bf7-4250-894c-508fb7a28109/Meth/")
-genes <- read.csv("genes_and_sites.csv",
-                  header = T)[2:8]
+genes <- read.csv("genes_and_sites.noreps.csv",
+                  header = T)[3:9]
 
 print("[GSEA] Gene set loaded")
 
 genes <- genes %>% 
-  dplyr::filter(P < 0.05 & !is.na(Gene)) %>%
+  dplyr::filter(P < 10^-10 & !is.na(Gene)) %>%
   group_by(Gene) %>% 
   summarise(Avg_Rho = mean(Rho))
 
@@ -31,11 +32,13 @@ GOCC <- dplyr::filter(
 
 print("[GSEA] Databases loaded")
 
+categories <- rbind(GOCC, GOBP, GOMF)
+
 ranks <- genes$Avg_Rho %>% as.numeric()
 names(ranks) <- genes$Gene
 
 grouped_categories <- categories %>% group_by(gs_exact_source)
-pathways <- group_map(grouped_categories, ~ .x[[1]])
+pathways <- group_map(grouped_categories, ~ .x[[4]])
 names(pathways) <- group_data(grouped_categories)[[1]]
 
 print("[GSEA] Preparations done. Starting GSEA")
@@ -45,5 +48,8 @@ gsea$leadingEdge <- vapply(gsea$leadingEdge, paste, collapse = ";", character(1L
 
 print("[GSEA] GSEA done")
 
+gsea_pval <- gsea %>% dplyr::filter(padj < 0.001)
+
 write.csv(gsea, "gsea_go.csv", quote = F)
+write.csv(gsea_pval, "gsea_go.pval.csv", quote = F)
 print("[GSEA] File written. Done")
